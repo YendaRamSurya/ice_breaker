4b1b9caeb724c1c38754779b0d2d036f63ab5923
YendaRamSurya <63895091+YendaRamSurya@users.noreply.github.com>
2024-09-01 19:51:19 -0500

    adding the files of react agent but it is depricated

---
+++ main.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+if __name__ == '__main__':
+    print("Hello React LangChain")
+    
+    /Users/ramsuryayenda/Documents/Project Space/ltphd/Libraries/Langchain_tutorial/Udemy_lanchain/main.py
